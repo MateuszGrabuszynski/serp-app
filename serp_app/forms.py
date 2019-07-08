6d33eba725f45ceeff27b558a9e3054e18b99119
mgrabuszynski <mgrabuszynski@gmail.com>
2019-07-08 03:36:26 +0200

    templates styling

--- serp_app/forms.py
+++ serp_app/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class NormalQueryForm(forms.Form):
     query = forms.CharField()
     no_results_to_return = forms.IntegerField()
-    user_agent = forms.CharField(widget=forms.Textarea(attrs={"rows": 5, "cols": 20}), required=False, help_text="Default UA will be used if empty")
+    user_agent = forms.CharField(widget=forms.Textarea(attrs={"rows": 5, "cols": 40}), required=False, help_text="Default UA will be used if empty")
     proxy_ip = forms.GenericIPAddressField(required=False, help_text="No proxy will be used if empty")
